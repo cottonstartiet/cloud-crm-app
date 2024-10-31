df5c3e2a8315b8e5a6b1d22d31d283dae95986b8
Aseem Gaurav <cottonstartiet@gmail.com>
2024-10-31 10:32:27 +0530

    Creating the Crm application API

---
+++ CrmApi/CrmApi/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CrmApi.Entities
+{
+    public class BaseEntity
+    {
+        [JsonPropertyName("id")]
+        public required string Id { get; set; }
+        [JsonPropertyName("pk")]
+        public required string PartitionKey { get; set; }
+    }
+}
---
+++ CrmApi/CrmApi/Entities/LeadModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CrmApi.Entities
+{
+    public class LeadModel : BaseEntity
+    {
+        [JsonPropertyName("firstName")]
+        public required string FirstName { get; set; }
+        [JsonPropertyName("lastName")]
+        public required string LastName { get; set; }
+        [JsonPropertyName("dob")]
+        public DateOnly DoB { get; set; }
+        [JsonPropertyName("primaryPhone")]
+        public required string PrimaryPhone { get; set; }
+        [JsonPropertyName("email")]
+        public required string Email { get; set; }
+        [JsonPropertyName("alternateEmail")]
+        public required string AlternameEmail { get; set; }
+    }
+}
