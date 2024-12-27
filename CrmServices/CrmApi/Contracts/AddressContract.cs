a123ef6d79fb3af5e36e53e91bff6ac8439808f0
Aseem Gaurav <cottonstartiet@gmail.com>
2024-12-27 20:29:45 +0530

    Validation sample

---
+++ CrmServices/CrmApi/Contracts/AddressContract.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrmApi.Contracts
+{
+    public class AddressContract
+    {
+        public required string Street { get; set; }
+        public required string State { get; set; }
+        [Required]
+        public required string Country { get; set; }
+
+    }
+}
--- CrmServices/CrmApi/Contracts/CreateContactRequest.cs
+++ CrmServices/CrmApi/Contracts/CreateContactRequest.cs
@@ -10,4 +10,8 @@ public record CreateContactRequest
     [Required]
     [Length(3, 50)]
     public required string LastName { get; init; }
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+    public AddressContract? ConactAddress { get; set; }
 }
